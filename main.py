04ae9ea0b9c7a41f229abb2b7862caa3cb3d11a0
cloudwithv <vincent.torrijos@cbre.com>
2024-12-03 17:06:50 -0600

    3rd commit

--- main.py
+++ main.py
@@ -8,32 +8,32 @@ username = os.getenv('EMAIL')
 password = os.getenv('PASSWORD')
 
 def connect_to_email():
-    mail = imaplib.IMAP4_SSL('smtp-mail.outlook.com')
+    mail = imaplib.IMAP4_SSL('imap-mail.outlook.com')
     mail.login(username, password)
     mail.select('inbox')
     return mail
 
 def search_for_email():
-        mail = connect_to_email()
-        _, search_data = mail.search(None, '(BODY "unsubscribe")')
-        data = search_data[0].split()
+    mail = connect_to_email()
+    _, search_data = mail.search(None, '(BODY "unsubscribe")')
+    data = search_data[0].split()
 
-        for num in data:
-            data = mail.fetch(num, '(RFC822)')
-            msg = email.message_from_bytes(data[0][1])
+    for num in data:
+        _, data = mail.fetch(num, '(RFC822)')
+        msg = email.message_from_bytes(data[0][1])
 
-            if msg.is_multipart():
-                for part in msg.walk():
-                    if part.get_content_type() == 'text/html':
-                        html_content = part.get_payload(decode=True).decode()
-                        print(html_content)
-            else:
-                content_type = msg.get_content_type()
-                content = msg.get_payload(decode=True).decode()
+        if msg.is_multipart():
+            for part in msg.walk():
+                if part.get_content_type() == 'text/html':
+                    html_content = part.get_payload(decode=True).decode()
+                    print(html_content)
+        else:
+            content_type = msg.get_content_type()
+            content = msg.get_payload(decode=True).decode()
 
-                if content_type == "text/html":
-                    print(content)
+            if content_type == "text/html":
+                print(content)
 
-        mail.logout()
+    mail.logout()
 
 search_for_email()
