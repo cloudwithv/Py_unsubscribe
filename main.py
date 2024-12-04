ee561f6240630a18659a6337599a8495e06efc42
cloudwithv <vincent.torrijos@cbre.com>
2024-12-04 16:46:35 -0600

    5th commit

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ username = os.getenv("EMAIL")
 password = os.getenv("PASSWORD")
 
 def connect_to_mail():
-    mail = imaplib.IMAP4_SSL("imap-mail.outlook.com")
+    mail = imaplib.IMAP4_SSL('imap.gmail.com')
     mail.login(username, password)
     mail.select('inbox')
     return mail
